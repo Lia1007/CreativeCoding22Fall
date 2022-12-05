adb5b3bf9af97b8c0ac70c7813f2fec429e3c354
Lia1007 <114713492+Lia1007@users.noreply.github.com>
2022-12-05 12:55:40 +0800

    Add files via upload

---
+++ Maxident musicplayer/application.windows64/source/Maxident_Player.java
@@ -0,0 +1,296 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import ddf.minim.analysis.*; 
+import ddf.minim.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Maxident_Player extends PApplet {
+
+
+//引用minim库，用来播放音乐
+Minim minim;
+PVector Position;
+PVector Velocity;
+ParticleSystem ps = new ParticleSystem(new PVector(width/2, 630));
+AudioPlayer[] music=new AudioPlayer[4];
+PImage base;//背景图片
+PImage kuang;//唱片框
+PImage heart; // 爱心
+PImage[] p=new  PImage[4];//专辑图片数组
+int index=0;//当前音乐
+float angle=0;
+float voice;//声明一个float变量，用来存储音乐的音量，因为我想做音乐交互~~~
+public void setup()
+{
+  
+  minim = new Minim(this);
+  base=loadImage("base.png");
+  base.resize(1181, 1574);
+  //kuang=loadImage("kuang.png");
+  for (int i=0; i<p.length; i++)
+  {   
+    p[i]=loadImage("p"+i+".png");
+    p[i].resize(750, 750);
+  }
+  for (int i=0; i<music.length; i++)
+  {
+    music[i]= minim.loadFile("music"+i+".mp3", 2048);
+  }
+  imageMode(CENTER);
+  music[index].play();
+  heart = loadImage("heart.png");
+  heart.resize(100, 100);
+}
+public void draw()
+{
+  voice = music[index].mix.level();
+  //背景
+  image(base, width/2, height/2);
+  pushMatrix();
+  translate(width/2, 630);//确定旋转中心
+  rotate(radians(angle));//旋转
+  //封面
+  image(p[index], 0, 0);
+  //唱片框
+  //image(kuang, 0, 0);
+  popMatrix();
+  if (music[index].isPlaying())
+  {
+    angle+=1;
+  }
+  //播放条
+  //未播放
+  noStroke();
+  fill(0xffE5E5E5);
+  rect(325, 1400, 800, 17, 20);
+
+  float pos = map(music[index].position(), 0, music[index].length(), 0, 1048);
+  if (pos>1044) {
+    music[index].rewind();
+    music[index].pause();
+    index+=1;
+    if (index==music.length)
+    {
+      index=0;
+    }
+    music[index].play();
+    angle=0;
+  }
+
+
+  //已播放
+  noStroke();
+  fill(0xff3A5977);
+  rect(324, 1400, pos, 19, 20);
+  //按钮
+  noStroke();
+  fill(0xffE9F4FF);
+  ellipse(pos+325, 1405, 32, 32);
+  fill(0xff3A5977);
+  ellipse(pos+325, 1405, 18, 18);
+
+  //暂停，前一首，后一首
+  //暂停
+  if (music[index].isPlaying())
+  {
+    noStroke();
+    fill(0xffE9F4FF);
+    rect(150, 1390, 7, 41, 4);
+    rect(170, 1390, 7, 41, 4);
+  } else {
+    noStroke();
+    fill(0xffE9F4FF);
+    triangle(150, 1390, 150, 1431, 180, 1410);
+  }
+  //左
+  noStroke();
+  fill(0xffE9F4FF);
+  rect(80, 1392, 4, 38, 2);
+  triangle(80, 1411, 110, 1392, 110, 1430);
+  //右
+  noStroke();
+  fill(0xffE9F4FF);
+  rect(240, 1392, 4, 38, 2);
+  triangle(240, 1411, 210, 1392, 210, 1430);
+
+  //控制进度
+  if (mousePressed&mouseX>325&&mouseX<1125&&mouseY>1399&&mouseY<1418) {
+    int position=(int)map(mouseX, 325, 1373, 0, music[index].length());
+    music[index].cue(position);
+  }
+
+  pushMatrix();
+  translate(width/2+580, height);
+  rotate(PI);
+  for (int i=0; i<40; i++)
+  {
+    fill(255);
+    rect(29*i, 0, 26, voice*300*noise(i));
+  }
+  popMatrix();
+
+  int minSize = 300;
+  int range = 130;  
+
+translate(width/2, height/2.5f);
+
+  for (int i=0; i<music[index].right.size(); i+=4) {
+    stroke(color(255 - sin(map(i, 0, music[index].right.size(), 0, 1) * PI) * 255, 0, sin(map(i, 0, music[index].right.size(), 0, 1) * PI) * 255));
+    float r = map(i, 0, music[index].right.size(), 0, 2 * PI);
+    float s = abs(music[index].right.get(i))*range;
+    line(sin(r) * (minSize), cos(r) * (minSize), sin(r) * (s + minSize), cos(r) * (s + minSize));
+  }
+
+}
+public void mousePressed()
+{
+  if (mouseX>149&&mouseX<171&&mouseY>1389&&mouseY<1432)
+  {
+    if (music[index].isPlaying())
+    {
+      music[index].pause();//暂停当前音乐
+    } else {
+      music[index].play();
+    }
+  } else if (mouseX>79&&mouseX<111&&mouseY>1389&&mouseY<1432)//切换到上一首
+  {
+    //切换歌曲需要做的事情
+    //1。当前歌曲退回到最开始的位置
+    //2.当前歌曲需要停下来
+    //唱片旋转角度复位
+    music[index].rewind();
+    music[index].pause();
+    index-=1;
+    if (index==-1) {
+      index=music.length-1;
+    }
+    music[index].play();
+    angle=0;
+  } else if (mouseX>209&&mouseX<241&&mouseY>1389&&mouseY<1432)
+  {
+    music[index].rewind();
+    music[index].pause();
+    index+=1;
+    if (index==music.length) {
+      index=0;
+    }
+    music[index].play();
+    angle=0;
+  } else if (mouseX>215&&mouseX<965&&mouseY>255&&mouseY<1005)
+  {
+  ps.DrawParticle();
+  }
+
+}
+class ParticleSystem {
+  ArrayList<Particle> pList = new ArrayList<Particle>();
+
+
+
+
+  PVector origin;
+
+  ParticleSystem(PVector location) {
+    origin = location.copy();
+    pList = new ArrayList();
+
+    for (int i = 0; i< 5; i++)
+    {
+      pList.add(new Particle(origin));
+    }
+  }
+
+public void DrawParticle() {
+
+    for (int i= pList.size()-1; i >= 0; i--)
+    {
+      Particle p = pList.get(i);
+      p.reload();
+      p.render();
+    }
+    addParticle();
+  }
+
+
+
+  public void addParticle() 
+  {
+
+    pList.add(new Particle(origin));
+  }
+}
+class Particle {
+
+  PVector location;
+  PVector velocity;
+  PVector acceleration;
+  float lifespan;
+  float alpha;
+
+  Particle() {
+    location = new PVector(width/2, 630);
+    acceleration = new PVector(random(-0.0002f, -0.02f), 0.02f);
+    velocity = new PVector(random(-4, 4), random(-4, 0));
+    lifespan = random(200-255);
+    alpha = 255;
+  }
+  
+  Particle(PVector _location) {
+    location = _location.copy();
+    acceleration = new PVector(random(-0.0002f, -0.02f), 0.02f);
+    //velocity = new PVector(random(-4, 4), random(-4, 0));
+        velocity = new PVector(random(-4,4), random(-4, 4));
+
+    lifespan = random(200-255);
+    alpha = 255;
+  }
+
+  public boolean finished() {
+    return alpha < 0;
+  }
+
+  public void reload() {
+    velocity.add(acceleration);
+    location.add(velocity);
+    lifespan -= -2.0f;
+    alpha -= 0;
+  }
+
+  public void render() {
+    stroke(0, lifespan);
+    fill(252, 173, 179, alpha);
+   
+   
+    tint(255,150);
+    image(heart,location.x-65,location.y-600);
+  }
+
+  public boolean isDead() {
+    if (lifespan < 0.0f) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+  public void settings() {  size(1181, 1574); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Maxident_Player" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
